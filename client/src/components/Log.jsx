1ebbe51afaa6b3d05846ba4248c6e56be2bcf390
samerr22 <laksameera5@gmail.com>
2024-03-31 12:18:13 +0530

    done

---
+++ client/src/components/Log.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div className=' w-full h-[50px] bg-[#ff834b] mt-0'>
+
+      <div className='flex justify-center items-center'></div>
+    </div>
+  )
+}
